f5f5a081f656ede8706bb5b358f649aad136c5e8
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-08-16 14:05:12 -0500

    Layout and global wrap in Gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,30 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { MenuItem, StyledHeader } from "../styles/components"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <StyledHeader>
+    <Link to="/">
+      <img src="https://i.postimg.cc/6q3pg48v/Logo.png" alt="logo" />
+    </Link>
+    <nav>
+      <ul>
+        <MenuItem margin>
+          <Link to="/">Products</Link>
+        </MenuItem>
+        <MenuItem margin>
+          <a href="https://www.platzi.com/">Platzi</a>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/cart">Cart</Link>
+          <span>
+            <img src="https://i.postimg.cc/L6wpMxLt/cart.png" alt="CartLogo" />
+          </span>
+        </MenuItem>
+      </ul>
+    </nav>
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -28,7 +37,7 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
-  </header>
+  </StyledHeader>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Layout, SEO, Jumbo } from "../components"
+import { SEO, Jumbo } from "../components"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
     <Jumbo />
     <h1>Hi people</h1>
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to /page 2</Link> <br />
     <Link to="/thanks/">Go to /thanks</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
