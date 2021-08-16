2940e04111fc60b00bfbddd68df57b6e1ae0aa1f
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-08-16 12:12:57 -0500

    Creating the jumbo component

---
+++ src/components/jumbo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import { StyledJumbo } from "../styles/components"
+
+const Jumbo = () => {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Consigue tu swag exclusivo!</h2>
+        <small>Piezas elegantes para los mejores estudiantes</small>
+      </div>
+    </StyledJumbo>
+  )
+}
+
+export default Jumbo
