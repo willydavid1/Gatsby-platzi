e7a167b3cbabfde728659e4d8389df2982ebe425
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-08-21 17:04:19 -0500

    Styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import { SEO, Jumbo } from "../components"
+import styled from "styled-components"
 
 // pass this information through props to the component
 export const query = graphql`
@@ -17,16 +18,27 @@ export const query = graphql`
   }
 `
 
-const IndexPage = ({ data, ...rest }) => {
+const Button = styled.button`
+  width: 10rem;
+  background-color: #98ca3f;
+  border: none;
+  border-radius: 10px;
+  color: ${({ color }) => color ?? "#ffffff"};
+  cursor: pointer;
+  transition: 1s;
+  &:hover {
+    transform: scale(1.1);
+  }
+`
+
+const IndexPage = ({ data }) => {
   return (
     <>
       <SEO title="Home" />
       <Jumbo
         description={data.allSite.edges[0].node.siteMetadata.description}
       />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
+      <Button>Button</Button> <br />
       <Link to="/page-2/">Go to /page 2</Link> <br />
       <Link to="/thanks/">Go to /thanks</Link>
     </>
